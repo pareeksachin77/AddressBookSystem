1fe3bae9a3bf08b2b6a85668863d463e797a2949
pareeksachin77 <pareeksachin77@gmail.com>
2022-10-19 22:15:18 +0530

    addressbookmaster

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class AddressBook
+    {
+        public void createContact()
+        {
+            Console.WriteLine("Enter First Name");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            string LastNme = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            string Address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            string city = Console.ReadLine();
+            Console.WriteLine("enter state");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter zip code");
+            int Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone number");
+            long PhoneNo = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter Email-id");
+            string Email = Console.ReadLine();
+
+        }
+    }
+}
---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AddressBookSystem;
+
+
+
+Console.WriteLine("Welcome to Address Book Program");
+
+bool check = true;
+
+while (check)
+{
+    Console.WriteLine("Please select the Options :\n" +
+        "1)Enter Details to add Contact in List\n");
+
+    int option = Convert.ToInt32(Console.ReadLine());
+
+    switch (option)
+    {
+        case 1:
+
+            AddressBook addobj = new AddressBook();
+            addobj.createContact();
+            break;
+
+        default:
+            Console.WriteLine("Please select proper option");
+            break;
+    }
+    check = false;
+    break;
+}
+        
